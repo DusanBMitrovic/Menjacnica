29d9062412e6d62e24639c0cfad0d3e1dfefcbea
Dusan Mitrovic <dule.mitrovic000@gmail.com>
2018-03-21 09:41:07 +0100

    InterfejsMenjacnica implementiran u klasi Menjacnica

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,10 +1,30 @@
 package menjacnica;
 import java.util.*;
 
+import menjacnicainterfejs.InterfejsMenjacnica;
 
-public class Menjacnica {
+
+public class Menjacnica implements InterfejsMenjacnica {
 	
 	LinkedList <Valuta> valute = new LinkedList<Valuta>();
+
+	@Override
+	public void dodajKurs(String naziv, double kursS, double kursP, double kursK, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ozbrisiKurs(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String vratiKurs(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	
 	
